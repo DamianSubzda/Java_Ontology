c1c9ff5b07a95259e0d2ed93701646dcd2f80404
Damian Subzda <damian.subzda@student.wat.edu.pl>
2022-12-03 00:24:26 +0100

    commit1

---
+++ src/main/java/main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class main extends JFrame {
+
+    public static void main(String [] args){
+        main m1 = new main();
+    }
+
+    main(){
+        setTitle("Ontology");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);;
+        pack();
+        setVisible(true);
+    }
+
+}
